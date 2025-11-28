5aecad40e07f6bb757625c89291a2f3fb22b622a
newsaisha7415 <newsaisha7415@gmail.com>
2025-11-28 19:00:35 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'scalping-cache-v1';
+
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles_clean.css',
+  '/main_clean.js',
+  '/dashboard.html',
+  '/images/icon-192.png',
+  '/images/icon-512.png'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request)
+      .then(resp => resp || fetch(evt.request))
+  );
+});
