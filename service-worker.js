9450c6e2a3b873db3d62ef8cdb78e627e248e563
newsaisha7415 <newsaisha7415@gmail.com>
2025-11-28 19:17:30 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,47 @@
 const CACHE_NAME = 'scalping-cache-v1';
 
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/styles_clean.css',
-  '/main_clean.js',
-  '/dashboard.html',
-  '/images/icon-192.png',
-  '/images/icon-512.png'
+  'index.html',
+  'dashboard.html',
+  'strategies.html',
+  'styles_clean.css',
+  'main_clean.js',
+  'tools.html',
+  'tradingview.html',
+  'manifest.json',
+  'service-worker.js',
+  'images/icon-192.png',
+  'images/icon-512.png'
 ];
 
 self.addEventListener('install', evt => {
   evt.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', evt => {
   evt.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(k => {
-          if (k !== CACHE_NAME) return caches.delete(k);
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
         })
-      )
-    )
+      );
+    })
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', evt => {
   evt.respondWith(
-    caches.match(evt.request)
-      .then(resp => resp || fetch(evt.request))
+    caches.match(evt.request).then(resp => {
+      return resp || fetch(evt.request);
+    })
   );
 });
